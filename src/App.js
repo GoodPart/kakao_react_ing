d8866dee87174779327b5799c4e246b73c1aa53c
GoodPart <pks940122@cttd.co.kr>
2020-10-30 06:57:39 +0900

    push

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { Component, setState } from 'react';
 import KakaoLogin from 'react-kakao-login';
 import Kakao from 'kakaojs'
 import './App.css';
@@ -10,16 +10,106 @@ class App extends Component {
     this.state= {
       id: '',
       name : '',
-      provider: ''
+      profileImg : ''
     }
   };
 
-  responseKakao = res => {
-    this.setState({
-      id: res,
-      name: res
-    })
-    console.log('responseData',res)
+  // responseKakao = res => {
+  //   this.setState({
+  //     id: res,
+  //     name: res
+  //   })
+  //   console.log('responseData',res)
+  // }
+
+  bringProfile = () => {
+    Kakao.API.request({
+      url: '/v2/user/me',
+      success: function(response) {
+        // this.state.setState({
+             console.log(response.properties.nickname)
+             console.log(response.properties.profile_image)
+             alert(response.properties.nickname)
+          // })
+          console.log(response);
+      },
+      fail: function(error) {
+          console.log(error);
+      }
+  });
+  }
+
+  //나에게 메세지 보내기
+  toMSGForMe = () => {
+    Kakao.API.request({
+  url: '/v2/api/talk/memo/default/send',
+  data: {
+    template_object: {
+      object_type: 'feed',
+      content: {
+        title: '카카오톡 링크 4.0',
+        description: '디폴트 템플릿 FEED',
+        image_url:
+          'http://mud-kage.kakao.co.kr/dn/Q2iNx/btqgeRgV54P/VLdBs9cvyn8BJXB3o7N8UK/kakaolink40_original.png',
+        link: {
+          web_url: 'https://developers.kakao.com',
+          mobile_web_url: 'https://developers.kakao.com',
+        },
+      },
+      social: {
+        like_count: 100,
+        comment_count: 200,
+      },
+      button_title: '바로 확인',
+    },
+  },
+  success: function(response) {
+    console.log(response);
+  },
+  fail: function(error) {
+    console.log(error);
+  },
+});
+
+Kakao.API.request({
+  url: '/v2/api/talk/memo/default/send',
+  data: {
+    template_object: {
+      object_type: 'feed',
+      content: {
+        title: '카카오톡 링크 4.0',
+        description: '디폴트 템플릿 FEED',
+        image_url:
+          'http://mud-kage.kakao.co.kr/dn/Q2iNx/btqgeRgV54P/VLdBs9cvyn8BJXB3o7N8UK/kakaolink40_original.png',
+        link: {
+          web_url: 'https://developers.kakao.com',
+          mobile_web_url: 'https://developers.kakao.com',
+        },
+      },
+      social: {
+        like_count: 100,
+        comment_count: 200,
+      },
+      button_title: '바로 확인',
+    },
+  },
+  success: function(response) {
+    console.log(response);
+  },
+  fail: function(error) {
+    console.log(error);
+  },
+});
+    
+  }
+
+  kakaoAuth = ()=> {
+    Kakao.Auth.authorize({
+      redirectUri: 'http://localhost:3000/oauth',
+      throughTalk: true
+      
+    });
+    console.log()
   }
   
 
@@ -28,12 +118,57 @@ class App extends Component {
       console.log("logout", res)
     })
   }
+  navi=()=> {
+    Kakao.Navi.start({
+      name: "현대백화점 판교점",
+      x: 127.11205203011632,
+      y: 37.39279717586919,
+      coordType: 'wgs84'
+  });
+  }
+  logOutExfire = () => {
+    Kakao.API.request({
+      url: '/v1/user/unlink',
+      success: function(response) {
+        console.log(response);
+      },
+      fail: function(error) {
+        console.log(error);
+      },
+    });
+  }
+
+  loginAuth = () => {
+    Kakao.Auth.authorize({
+      redirectUri: 'http://localhost:3000/oauth',
+      //한번 정보동의 및 회원가입하면 다음부터 별도의 동의없이 간편 로그인 기능임
+      throughTalk: true
+    });
+  }
 
   logIn =() => {
+    //카카오 로그인 시작
     Kakao.Auth.login({
 
       success :function(response) {
-        console.log("res", response)
+        // console.log("res", response)
+
+        //리다이렉트 받을 곳을 지정한다.
+        Kakao.Auth.authorize({
+          redirectUri: 'http://localhost:3000/oauth',
+          //한번 정보동의 및 회원가입하면 다음부터 별도의 동의없이 간편 로그인 기능임
+          throughTalk: true
+        });
+
+      //   Kakao.API.request({
+      //     url: '/v2/user/me',
+      //     success: function(res) {
+      //       console.log("me", res)
+      //     }
+      // });
+
+
+        
   
         // Kakao.Auth.getStatus(function(response){
   
@@ -94,6 +229,9 @@ class App extends Component {
   failKakao = err => {
     console.error(err)
   }
+  
+  
+
 
   render() {
     return(
@@ -106,13 +244,30 @@ class App extends Component {
         getProfile="true"
         /> */}
 
-        <h1 onClick={this.logIn}>
+        <h1 onClick={this.loginAuth}>
           로그인
         </h1>
-        
 
+        <h2 onClick={this.bringProfile}>
+          정보가져오기
+        </h2>
+
+        <h2 onClick={this.toMSGForMe}>
+          메세지 전송
+        </h2>
+
+        <h1 onClick={this.navi}>
+          <img
+            src="https://developers.kakao.com/assets/img/about/buttons/navi/kakaonavi_btn_medium.png"
+          />
+          {/* 네비 */}
+        </h1>
+        
         <h1 onClick={this.logOut}>
           로그아웃
+        </h1>
+        <h1 onClick={this.logOutExfire}>
+          연결 해제
         </h1>
       </>
     )
